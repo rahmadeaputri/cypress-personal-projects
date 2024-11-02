ff36480864d66438862c2daeee5ac83ed870a315
rahmadeaputri <rahmadeaputri02@gmail.com>
2024-11-03 00:20:53 +0700

    second commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseURL: "https://katalon-demo-cura.herokuapp.com/",
+    baseUrl: "https://katalon-demo-cura.herokuapp.com/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/curaHealthcareService.cy.js
+++ cypress/e2e/curaHealthcareService.cy.js
@@ -7,7 +7,7 @@ This project uses the website from https://katalon-demo-cura.herokuapp.com/
 describe('Cura Make Appointment - Positive', () => {
 
     it('Visit the URL', () => {
-        cy.visit(Cypress.env('baseURL'))
+        cy.visit("https://katalon-demo-cura.herokuapp.com/")
         cy.get("#top").contains("CURA Healthcare Service") // verify that already in this website
     })
 
@@ -18,24 +18,22 @@ describe('Cura Make Appointment - Positive', () => {
 
     })
 
-    it('Login', () => {
+    it('Login & Make Appointment & Appointment Confirmation', () => {
+        cy.visit("https://katalon-demo-cura.herokuapp.com/profile.php#login")
         cy.get("#txt-username").type("John Doe") //id  
         cy.get("#txt-password").type("ThisIsNotAPassword") //id 
         cy.get("#btn-login").click() //id 
         cy.get(".form-group").contains("Facility") // verify that already in make appointment page
-    })
 
-    it('Make Appointment', () => {
         cy.get("#combo_facility").select("Tokyo CURA Healthcare Center")
         cy.get("#chk_hospotal_readmission").check()
-        cy.get(".radio-inline[value='Medicare']").should('be.checked')
+        cy.get("#radio_program_medicare").click()
         cy.get("#txt_visit_date").type("07/11/2024")
+        cy.get('#txt_comment').click({ force: true })
         cy.get("#txt_comment").type("I will be avaiable at 07/11/2024")
         cy.get("#btn-book-appointment").click()
-    })
 
-    it('Appointment Confirmation', () => {
-        cy.get(".col-xs-12 text-center").contains("Appointment Confirmation")
+        cy.get(".col-xs-12.text-center").contains("Appointment Confirmation")
         cy.get("#visit_date").contains("07/11/2024")
         cy.get("#comment").contains("I will be avaiable at 07/11/2024")
     })
